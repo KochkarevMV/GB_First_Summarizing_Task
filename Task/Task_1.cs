5d10cd2da3d014dd2c025d231ad58015e237cdd2
KochkarevMV <ufomusic@mail.ru>
2023-03-08 16:40:19 +0300

    C# Task done in Task_1.cs

--- Task/Task_1.cs
+++ Task/Task_1.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк
+// формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+
+
+Console.Clear();
+string [] array1 = {"ABC", "ABCD", "XYZ", "WXYZ", "123", "12345"};
+Console.WriteLine($"Начальный массив: {string.Join(" ", array1)}");
+
+int LimitCheck = 3;
+string [] array2 = new string [array1.Length];
+int k = 0;
+for (int i = 0; i < array1.Length; i++)
+{
+    if (array1[i].Length <= LimitCheck)
+    {
+        array2[k] = array1[i];
+        k++;
+    }
+}
+Console.Write($"Конечный массив: {string.Join(" ", array2)}");
